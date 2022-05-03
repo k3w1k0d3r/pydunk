5bb21f91704a0023247ecf5d2e7b016bf9cd1daa
Julien Grijalva <juliengrijalva@gmail.com>
2022-05-03 16:44:10 -0600

    gdfs

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+	name="pyDunk",
+	version="0.0.1",
+	author="k3w1k0d3r",
+	author_email="juliengrijalva@gmail.com",
+	packages=["pydunk"],
+	scripts=[],
+	url="",
+	license='LICENSE.txt',
+	description="dunkin donuts with python",
+	long_description=open("README.txt").read(),
+	install_requires=[
+		"geopy",
+	],
+)
